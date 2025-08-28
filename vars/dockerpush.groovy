224c4a5c36c26bfb6586ba70c0a04a50f5551576
Kalim Nawaz <96150072+Kalimnawaz@users.noreply.github.com>
2025-08-29 01:21:51 +0530

    Update dockerpush.groovy

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,8 +1,8 @@
-def call(String Project, String ImageTag String dockerhubuser){
+def call(String Project, String ImageTag, String dockerhubuser){
 withCredentials([usernamePassword(credentialsId: 'dockerhubcred', usernameVariable: 'dockerhubuser', passwordVariable: 'dockerhubpass')]) {
                 sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
                 sh "docker image tag notes-app:latest ${env.dockerhubuser}/notes-app:latest"
-}
                 sh "docker push ${env.dockerhubuser}/notes-app:latest"
+}
 }
 
